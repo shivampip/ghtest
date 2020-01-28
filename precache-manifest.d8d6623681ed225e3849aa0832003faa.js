b6215b76eeb27cfc080f17163779e0109bfb04c9
Shivam Agrawal <shivam301296@gmail.com>
2020-01-28 12:56:31 +0530

    Updates

---
+++ precache-manifest.d8d6623681ed225e3849aa0832003faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dc1c887bbf55d8d8bf9a5842e0f033",
+    "url": "/ghtestt/index.html"
+  },
+  {
+    "revision": "48a51aca12a4d03ba0cd",
+    "url": "/ghtestt/static/css/main.6227d6d1.chunk.css"
+  },
+  {
+    "revision": "fd68c3de00656901c31b",
+    "url": "/ghtestt/static/js/2.623fc69e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ghtestt/static/js/2.623fc69e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48a51aca12a4d03ba0cd",
+    "url": "/ghtestt/static/js/main.ca89b3d1.chunk.js"
+  },
+  {
+    "revision": "1712acddbb1900047c8c",
+    "url": "/ghtestt/static/js/runtime-main.a1d29465.js"
+  }
+]);
