07490928c3b24cd24a70974df47949b944b7b4ef
shivampip <shivam301296@gmail.com>
2020-01-28 12:46:56 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+const App= ()=>{
+    return (
+        <div className="app">
+            <p>Congratulations</p>
+            <p>You have successfully deployed React app on Github Pages</p>
+            <a href="https://pages.github.com/" target="_blank"><button>Know more about Github Pages</button></a><br/>
+            <a href="https://github.com/gitname/react-gh-pages" target="_blank"><button>Knwo more about gh-pages</button></a><br/>
+            <a href="https://github.com/shivampip/" target="_blank"><button>Follow me on Github</button></a>
+        </div>
+    ); 
+}
+
+export default App;
